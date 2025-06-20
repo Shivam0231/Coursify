6a3afb77de802a7c9e0eb519e7fce7915b23e731
shivam singh <shivamsingh916582@gmail.com>
2025-06-21 00:13:06 +0530

    Add nodemon and update config and App.jsx

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ dotenv.config(); // ✅ This line loads .env variables into process.env
 
 const JWT_SECRET = process.env.JWT_SECRET; // ✅ Declare the variable properly
 const admin_jwt_secret = process.env.ADMIN_JWT_SECRET; // ✅ Declare the variable properly
-const stripe_SECRET_KEY ="sk_test_51RZysrDClBJKAEx8UBaUAfjuOsNKZ7uHDwfHSOetGsqO38PpKoaE0AQ7WmmzvV6U7mqKfusUztq8AmNBh1FsZWbg002UXNafbQ"
+const stripe_SECRET_KEY =process.env.STRIPE_SECRET_KEY
 export default {
     JWT_SECRET,
     admin_jwt_secret,
